e7fd051db1ae9c56c341c38c1c0f9ebe27b79344
ananyasaigal001 <50635283+ananyasaigal001@users.noreply.github.com>
2020-05-30 13:23:47 -0400

    Create Adventure_Story.py

---
+++ Adventure_Story.py
@@ -0,0 +1,17 @@
+'''
+Choose your own adventure story game
+```
+
+#intro
+print("Your family is not normal, it never was,you never had many friends or much of a social life.")
+print("You live in small town where everyone knows each other. Everyone judges you for your dysfunctional family.")
+print("However things became worse when the police came to arrest your father. The next day you woke up to a series of negative messages on your social media accounts.")
+print("When you arrived at school everyone was gossiping about you.")
+print("At recess your best friend was laughing at you with other people,when you walked past her she wouldn’t make eye contact with you.")
+
+
+
+
+
+
+
