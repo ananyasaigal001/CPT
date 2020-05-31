71d4761d4019ea845d64c372f48c8199aa48a0b8
ananyasaigal001 <50635283+ananyasaigal001@users.noreply.github.com>
2020-05-30 20:18:47 -0400

    Update Adventure_Story.py

--- Adventure_Story.py
+++ Adventure_Story.py
@@ -1,21 +1,19 @@
 '''
 Choose your own adventure story game
 '''
-
+#function iterates over dictionary to give the user choices to pick
+def choice(dict):
+    for key,value in dict.items():
+        print(key,value)
 #intro
 print("Your family is not normal, it never was,you never had many friends or much of a social life.")
 print("You live in small town where everyone knows each other. Everyone judges you for your dysfunctional family.")
-print("However things became worse when the police came to arrest your father. The next day you woke up to a series of negative messages on your social media accounts.")
+print("However things became worse when the police came to arrest your father.")
+print("The next day you woke up to a series of negative messages on your social media accounts.")
 print("When you arrived at school everyone was gossiping about you.")
 print("At recess your best friend was laughing at you with other people,when you walked past her she wouldn’t make eye contact with you.")
 
-# dictionary function
-def choice(dict):
-'''function iterates through dictionary to print the choices for the user to choose from'''
-    for key,value in dict.items():
-        print(key,value)
-
-#choice one
+ #choice one
 choice_one={'option a:':'You go up to your friend and say Hi','option b:':'You ignore them and walk away','option c:':'You go to the washroom and cry'}
 option_one=choice(choice_one)
 
