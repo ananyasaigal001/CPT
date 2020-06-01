577eb26de0082c19057fefa0c252d0c44d28da16
ananyasaigal001 <50635283+ananyasaigal001@users.noreply.github.com>
2020-06-01 02:56:37 -0400

    Update Adventure_Story.py

--- Adventure_Story.py
+++ Adventure_Story.py
@@ -165,3 +165,30 @@ elif(choice_five =="b"):
   print("You inch the knife into your chest. Should I do this you ask yourself.")
   print("Deep down inside you hesitate once more.Then you remember how quickly the pain will end and you’ll finally be free.")
   print("You push the knife hard into your chest delivering one final scream and then its over.")
+
+#result from choice 6
+if (choice_six=="a"):
+    print("Slowly things become alright, your dad will be out of the prison in 2 months.")
+    print("Your mom has found a nice job with which she can easily run the home.")
+    print("A major cause of the family issues was the financial crisis your family was facing but the financial crisis will soon be resolved ")
+    print("There is a new girl in school who just moved into the area.She does not judge you for your family.")
+    print("You get along with her and have become good friends. You are no longer lonely and have found a new best friend.")
+
+
+elif (choice_six=="b"):
+    print("On the weekend you question everything about yourself.")
+    print("Your father is in prison, your mother can’t see the pain your in, your grades are falling and everyone at your school hates you.")
+    print("You feel lost there is only one option you see. Something that will finally take the pain away.")
+    print("You reach into the kitchen cupboard and pull it your sharpest knife.")
+    print("You hesitate, you want to tell everyone you loved a final goodbye.")
+    print("You cut into your flesh and form two letters with your blood.One to your mom and one to your best friend. ")
+    print("In the letter to your mom, you tell her that it is not her fault that you are doing this.")
+    print("You tell her,that this is the best for everyone.")
+    print("In the letter to your best friend, you tell her that you forgive her.")
+    print("You tell her that in these last days,you have not remembered her for the bad things she has done to you.")
+    print("But for all the happy memories together.")
+    print("You remind her of some of the good memories and then you end the letter.")
+    print("Then,you go out into backyard careful not to wake your mother.")
+    print("You inch the knife into your chest. Should I do this you ask yourself.")
+    print("Deep down inside you hesitate once more.Then you remember how quickly the pain will end and you’ll finally be free.")
+    print("You push the knife hard into your chest delivering one final scream and then its over.")
