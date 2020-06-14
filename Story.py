8b9b7ac5ac2f3a3d2cedcf7e80aeb40ed2bf080d
ananyasaigal001 <50635283+ananyasaigal001@users.noreply.github.com>
2020-06-14 17:26:45 -0400

    Choose your own story

--- Story.py
+++ Story.py
@@ -10,6 +10,23 @@ def choice(d):
 def output(l):
     for sentence in l:
         print(sentence)
+#code used later on
+choice_five = "x"
+choice_six = "y"
+choice_2_ab={'a:':'You tell the teacher what happened','b:':'You tell the teacher that you have dust in your eye'}
+choice_list=["a: You ignore them and keep your head down","b: You tell them to stop laughing"]
+finale=["On the weekend you question everything about yourself.",
+    "Your father is in prison, your mother can’t see the pain your in, your grades are falling and everyone at your school hates you.",
+    "You feel lost there is only one option you see. Something that will finally take the pain away.","You reach into the kitchen cupboard and pull it your sharpest knife.",
+    "You hesitate, you want to tell everyone you loved a final goodbye.",
+    "You cut into your flesh and form two letters with your blood.One to your mom and one to your best friend. ",
+    "In the letter to your mom, you tell her that it is not her fault that you are doing this.","You tell her,that this is the best for everyone.",
+    "In the letter to your best friend, you tell her that you forgive her.",
+    "You tell her that in these last days,you have not remembered her for the bad things she has done to you.",
+    "But for all the happy memories together.","You remind her of some of the good memories and then you end the letter.",
+    "Then,you go out into backyard careful not to wake your mother.","You inch the knife into your chest. Should I do this you ask yourself.",
+    "Deep down inside you hesitate once more.Then you remember how quickly the pain will end and you’ll finally be free.",
+    "You push the knife hard into your chest delivering one final scream and then its over."] 
 
 #intro
 age=int(input("Enter a age which is included in the teenager category(don't write the age in words):"))
@@ -28,9 +45,6 @@ option_one=choice(choice_1)
 #choose which option
 choice_one = input("Which option do you pick a,b or c? ")
 
-choice_two = "c"
-choice_six = "a"
-
 
 #results from first choice
 if(choice_one == "a"):
@@ -44,12 +58,10 @@ if(choice_one == "a"):
   print("You quickly wipe away your tears and run out leaving your diary behind.")
   print("You  start breaking down in class in front of the strictest teacher.") 
   print("He comes up and asks you whats wrong.")
-#choice 2 (a,b)
-choice_2_ab={'a:':'You tell the teacher what happened','b:':'You tell the teacher that you have dust in your eye'}
-option_2_ab=choice(choice_2_ab)
+  #choice 2 (a,b)
+  option_2_ab=choice(choice_2_ab)
 
-
-if(choice_one == "b"):
+elif(choice_one == "b"):
   print("You walk past them to a lonely table in the corner where you sit alone and do your math homework.")
   print("You have no one to talk to and you feel lonely. All around you, you see people laughing with their friends and having fun.")
   print("You get out your phone to check the time when you see a meme of yourself in handcuffs saying “Like father like daughter”.")
@@ -65,17 +77,12 @@ elif(choice_one == "c"):
   print("Your eyes are puffy, and red you can’t stop crying and struggle to pull yourself together.")
   print("You hear the bell ring and rush to your classroom forgetting your diary behind.")
   print("You only realize when the class has already started. The teacher is really strict and probably won’t let you leave to get it.")
-
   #choice 2
   choice_2={'a:':'You ask to go to the washroom','b:':'You quickly rush out of the class to get it'}
   option_2=choice(choice_2)
   choice_two = input("Which option do you pick a or b? ")
-
-
-
-if(choice_two == "a" or choice_two == "b" ):
+  #output
   print("Your teacher stops you from going to the washroom and you remain in class. He sees your eyes are puffy and asks you whats wrong.")
-
   #choice 2 (a,b)
   option_2_ab=choice(choice_2_ab)
 
@@ -87,11 +94,9 @@ if(choice_two_ab == "a"):
   print("You tell the teacher that people have been making fun of you because of your family.")
   print("Your teacher looks uncomfortable and tells you to come see him after class.")
   print("People around you start whispering about you being a snitch.")
-  print("You see a group of people including your best friend looking at a book and laughing.")
-  
-#choice 3
-choice_list=["a: You ignore them and keep your head down","b: You tell them to stop laughing"]
-option_3=output(choice_list)
+  print("You see a group of people including your best friend looking at a book and laughing.") 
+  #choice 3
+  option_3=output(choice_list)
 
 if(choice_two_ab =="b"):
   print("Your teacher looks relieved and continues the lesson.")
@@ -120,26 +125,22 @@ if(choice_four == "a" or choice_four == "b"):
   print("Your diary is nowhere to be found so you go home and start on your homework.")
   print("In the evening you see a bunch of posts showing intimate lines from your diary. ")
   print("You see a series of messages making fun of what you wrote and there was a group chat started to ridicule you further.")
-
   #choice 5
   choice_5={'a:':'You tell your Mom what is going on','b:':'You do nothing'}
   option_5=choice(choice_5)
-  
-  #choose an option
   choice_five = input("Which option do you pick a,b? ")
-  
 #results for choice 5
 if(choice_five =="a"):
-  print("Your Mom refuses to listen your problems as she is facing her own  financial difficulties and tells you not to worry and says its normal.You go back to doing your homework. ")
-  print("The next day you talk to your favourite teacher about your issues and she promises to talk to the students and tells you not to worry and everything will be fine. ")
+  print("Your Mom refuses to listen your problems as she is facing her own financial difficulties.") 
+  print("She tells you not to worry and says its normal.You go back to doing your homework.")
+  print("The next day you talk to your favourite teacher about your issues and she promises to talk to the students.")
+  print("She assures you that everything will be fine and tells you not to worry.")
   print("After the conversation you feel relieved and know that now everything will be fine. ")
   print("A couple days later the messages have stopped and the teacher asks you if you are fine")
 
   #choice 6
   print("a:","You tell her you are fine and thank her and give her a present")
   print("b:","You tell her you are miserable and everything is breaking apart")
-
-  # choose an option
   choice_six = input("Which option do you pick? ")
 
 elif(choice_five =="b"):
@@ -149,20 +150,8 @@ elif(choice_five =="b"):
   print("As your phone keeps buzzing with more and more horrible jokes you dig yourself into a hole filled with isolation and depression.")
   print("Eventually you get to a point where you can’t take it anymore.")
   print("A couple days later the messages reach to a point where they are hating on everything about you.")
-finale=["On the weekend you question everything about yourself.",
-    "Your father is in prison, your mother can’t see the pain your in, your grades are falling and everyone at your school hates you.",
-    "You feel lost there is only one option you see. Something that will finally take the pain away.","You reach into the kitchen cupboard and pull it your sharpest knife.",
-    "You hesitate, you want to tell everyone you loved a final goodbye.",
-    "You cut into your flesh and form two letters with your blood.One to your mom and one to your best friend. ",
-    "In the letter to your mom, you tell her that it is not her fault that you are doing this.","You tell her,that this is the best for everyone.",
-    "In the letter to your best friend, you tell her that you forgive her.",
-    "You tell her that in these last days,you have not remembered her for the bad things she has done to you.",
-    "But for all the happy memories together.","You remind her of some of the good memories and then you end the letter.",
-    "Then,you go out into backyard careful not to wake your mother.","You inch the knife into your chest. Should I do this you ask yourself.",
-    "Deep down inside you hesitate once more.Then you remember how quickly the pain will end and you’ll finally be free.",
-    "You push the knife hard into your chest delivering one final scream and then its over."]
-statement=output(finale)
-
+  statement=output(finale)
+  
 #result from choice 6
 if (choice_six=="a"):
     print("Slowly things become alright, your dad will be out of the prison in 2 months.")
@@ -170,8 +159,7 @@ if (choice_six=="a"):
     print("A major cause of the family issues was the financial crisis your family was facing but the financial crisis will soon be resolved ")
     print("There is a new girl in school who just moved into the area.She does not judge you for your family.")
     print("You get along with her and have become good friends. You are no longer lonely and have found a new best friend.")
-
-
-elif (choice_six=="b"):
-    ending=output(finale)
     
+
+elif (choice_six=="b"): 
+  ending=output(finale)
